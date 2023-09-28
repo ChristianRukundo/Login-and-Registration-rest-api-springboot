5bbde15bdf51d155a58690ca9c58058a1395eda0
ChristianRukundo <125260857+ChristianRukundo@users.noreply.github.com>
2023-09-28 21:19:24 +0200

    Update BlogAPIException.java

--- src/main/java/com/springboot/api/exception/BlogAPIException.java
+++ src/main/java/com/springboot/api/exception/BlogAPIException.java
@@ -6,6 +6,7 @@ public class BlogAPIException extends RuntimeException {
 
     private HttpStatus status;
     private String message;
+    
 
     public BlogAPIException(HttpStatus status, String message) {
         this.status = status;
